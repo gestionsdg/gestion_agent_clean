073ec36da9aa55e8a1e7cf00f47ccd4542b36808
gestionsdg <rogermavinga71@gmail.com>
2025-07-19 07:52:40 +0100

    Ajout route de test /test/

--- gestion_agent/urls.py
+++ gestion_agent/urls.py
@@ -38,3 +38,5 @@ urlpatterns += [
 # Fichiers statiques (en production)
 if not settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
+    path('test/', lambda request: HttpResponse("OK"), name='test'),
