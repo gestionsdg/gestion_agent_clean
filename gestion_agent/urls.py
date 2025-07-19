b6f6ca8b13a33543cfc9ed76b7243d0ebf9bf118
gestionsdg <rogermavinga71@gmail.com>
2025-07-19 06:49:18 +0100

    Correction ImportError: ajout de 'views' dans urls.py

--- gestion_agent/urls.py
+++ gestion_agent/urls.py
@@ -3,10 +3,11 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+
+from personnel import views  # ✅ Ajoutez cette ligne !
 from personnel.views_dashboard import tableau_de_bord
 from django.contrib.auth import views as auth_views
-from personnel.views import accueil  # Vue d'accueil personnalisée
-from personnel import views
+from personnel.views import accueil
 
 urlpatterns = [
     # Accueil
