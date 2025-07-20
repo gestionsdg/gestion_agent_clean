899c45841a8ea9bf0395e6d03465506d462cc766
gestionsdg <rogermavinga71@gmail.com>
2025-07-20 18:02:33 +0100

    Correction import explicite dans urls_filtrage.py pour Render

--- personnel/urls_filtrage.py
+++ personnel/urls_filtrage.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from personnel import views_export
+from personnel import views_export  # ✅ correction ici
 
 urlpatterns = [
-    path('retraitables/', views_export.liste_retraites, name='liste_retraites'),
-    path('demissionnaires/', views_export.liste_demis, name='liste_demis'),
-    path('detaches/', views_export.liste_detaches, name='liste_detaches'),
-    path('licencies/', views_export.liste_licencies, name='liste_licencies'),
-    path('disponibles/', views_export.liste_disponibilites, name='liste_disponibilites'),
+    path('actifs_par_entite/', views_export.liste_actifs_par_entite, name='liste_actifs_par_entite'),
     path('decedes/', views_export.liste_decedes, name='liste_decedes'),
-    path('responsables/', views_export.liste_responsables_par_entite, name='liste_responsables_par_entite'),
+    path('retraites/', views_export.liste_retraites, name='liste_retraites'),
+    path('licencies/', views_export.liste_licencies, name='liste_licencies'),
+    path('demis/', views_export.liste_demis, name='liste_demis'),
+    path('detaches/', views_export.liste_detaches, name='liste_detaches'),
+    path('disponibilites/', views_export.liste_disponibilites, name='liste_disponibilites'),
     path('controleurs/', views_export.liste_controleurs, name='liste_controleurs'),
+    path('responsables/', views_export.liste_responsables_par_entite, name='liste_responsables_par_entite'),
 ]
